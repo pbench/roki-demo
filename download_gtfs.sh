ba3dfe392a5c2a0388386bad49ea2eb912c9ec51
Pascal Benchimol <pascal.benchimol@polytechnique.edu>
2025-03-10 22:49:44 +0100

    create gh-pages branch

--- download_gtfs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl https://eu.ftp.opendatasoft.com/sncf/plandata/export_gtfs_voyages.zip --output data/tgv.zip
-curl https://eu.ftp.opendatasoft.com/sncf/plandata/export-ter-gtfs-last.zip --output data/ter.zip
-curl https://eu.ftp.opendatasoft.com/sncf/plandata/export-intercites-gtfs-last.zip --output data/intercites.zip
-
